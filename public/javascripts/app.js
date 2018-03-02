48373f68c33e497f5a053c27786a7a66dfc4b675
DavidWei95036593 <dw36593@pausd.us>
2018-03-02 12:30:31 -0800

    Finish the project

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,9 +1,48 @@
 
 $(document).ready(function(){
+  console.log("hello");
   const titleinput = $(`#movie-title`);
+  const poster =$(`#poster`);
+  const errorMessage = $(`#error-message`);
+  const dvd = $(`#DVD`);
+  const director = $(`#Director`);
+  const writer = $(`#Writer`);
+  const actor = $(`#Actor`);
   titleinput.on(`keyup`,function(e){
     if(e.key===`Enter`){
-    console.log(e);}
+
+      // window.location.replace(`/movie`);
+      const movieTitle = titleinput.val();
+      titleinput.val(``);
+      $.get(`/movieInfo?title=${movieTitle}`,(response)=>{
+        if(response.poster){
+          // console.log(response);
+          writer.removeClass(`hidden`);
+          actor.removeClass(`hidden`);
+          director.removeClass(`hidden`);
+          dvd.removeClass(`hidden`);
+          poster.removeClass(`hidden`);
+          errorMessage.addClass(`hidden`);
+          poster.attr(`src`,response.poster);
+          dvd.text(`The release date is ` +`${response.DVD}`);
+          director.text(`The Director/s is/are `+`${response.Director}`);
+          actor.text(`The Actors are `+`${response.Actor}`);
+          writer.text(`The Writer is `+`${response.Writer}`);
+          // poster.attr(response.)
+        }else{
+
+          errorMessage.removeClass(`hidden`);
+          errorMessage.text(`Could not find the poster for
+            ${movieTitle}`);
+            poster.addClass(`hidden`);
+            dvd.addClass(`hidden`);
+            director.addClass(`hidden`);
+            writer.addClass(`hidden`);
+            actor.addClass(`hidden`);
+        }
+      })
+      // console.log(`enter password`);
+  }
   })
   // console.log(titleinput);
 })
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require(`express`);
 const hbs = require(`hbs`);
 const path = require(`path`);
-
+const axios = require(`axios`);
 const app = express();
 
 app.set(`views`,path.join(__dirname,`/views`));
@@ -12,6 +12,28 @@ app.use(express.static(path.join(__dirname,"public")));
 app.get(`/`,(req,res)=>{
   //app.set(view engine )has already set the default of every render to hbs
   res.render(`index`);
+})
+app.get(`/movieInfo`,(req,res)=>{
+  const title = req.query.title;
+  const api_id="tt3896198";
+  const apiKey = "2e4ddc07";
+  axios.get(`https://omdbapi.com/?apikey=${apiKey}&t=${title}`)
+  .then((response)=>{
+    // console.log(response);
+    const Writer = response.data.Writer;
+    const poster = response.data.Poster;
+    const DVD = response.data.DVD;
+    const Director = response.data.Director;
+    const Actor = response.data.Actors;
+    // console.log(Actor);
+    res.send({poster,DVD,Director,Writer,Actor});
+    // res.send({DVD);
+
+  }).catch((response)=>{
+    res.send({});
+  })
+
+
 
 })
 app.listen(`3000`,()=>{
